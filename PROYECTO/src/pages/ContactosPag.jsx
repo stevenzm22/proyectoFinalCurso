21a5cb65a8fde8fbc20409c9925b9c0b22f50ef2
stevenzm22 <stevenjosez03@gmail.com>
2025-06-16 08:14:19 -0600

    segundo avance

--- PROYECTO/src/pages/ContactosPag.jsx
+++ PROYECTO/src/pages/ContactosPag.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import ContactosOficial from '../componentes/CONTACTOS/ContactosOficial'
-import NavContactos from '../componentes/NAV/NavContactos'
+
 
 function ContactosPag() {
   return (
     <div>
-      <NavContactos/>
+      
       <ContactosOficial/>
 
     </div>
--- PROYECTO/src/pages/SobreNosotros.jsx
+++ PROYECTO/src/pages/SobreNosotros.jsx
@@ -1,13 +1,12 @@
 import React from 'react'
 import Contactos from '../componentes/SOBRENOSOTROS/Contactos'
 import Footer from '../componentes/FOOTER/Footer'
-import NavSobreNosotros from '../componentes/NAV/NavSobreNosotros'
 
 
 function SobreNosotros() {
   return (
     <div>
-     <NavSobreNosotros/>
+    
      <br />
      <Contactos/>
       <br />
